660a1fbe0ea4dfb76c4126be564b9d61c5795d51
LearningMasterA <ankitasingh0263@gmail.com>
2025-09-13 18:35:48 +0530

    add_contact page added

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -14,9 +14,11 @@ import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuer
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.smart.dao.UserRepository;
+import com.smart.entities.Contact;
 import com.smart.entities.User;
 
 @Controller
@@ -26,19 +28,42 @@ public class UserController {
 	@Autowired
 	private UserRepository repo;
 	
-	@RequestMapping("/index")
-	public String dashboard(Model m,Principal principal) {
+	
+//	Method to add ommon data
+	@ModelAttribute
+	public void addCommonData(Model m,Principal principal) {
 		String username=principal.getName();
 		System.out.println(username);
 		User user = this.repo.getUserByUserName(username);
 		System.out.println(user);
 		m.addAttribute("user", user);
+	}
+	
+//	Dashboard Home
+	@RequestMapping("/index")
+	public String dashboard(Model m,Principal principal) {
+//		String username=principal.getName();
+//		System.out.println(username);
+//		User user = this.repo.getUserByUserName(username);
+//		System.out.println(user);
+//		m.addAttribute("user", user);
 		return "normal/user_dashboard";
 	}
 	
 	@RequestMapping("test")
 	public String home() {
 		return "normal/test";
+	}
+	
+	
+//	Open add contact form handler
+	
+	@RequestMapping("/add_contact")
+	public String openAddContactForm(Model m) {
+		m.addAttribute("title", "User");
+		m.addAttribute("contact",new Contact());
+		
+		return "normal/add_contact";
 	}
 
 }
